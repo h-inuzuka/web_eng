49e4f67203a2575a9c9853f561525bbe33553d27
inuzuka <h-inuzuka@biglobe.co.jp>
2014-09-03 00:12:09 +0900

    composer設定、slim初期表示

--- web/index.php
+++ web/index.php
@@ -1,2 +1,14 @@
 <?php
-    echo "index".PHP_EOL;
+//ライブラリの読み込み
+require '../vendor/autoload.php';
+
+//Slimの初期化
+$app = new \Slim\Slim([]);
+
+//Slimにルートを灯籠
+$app->get('/', function(){
+    echo "Hello, World!";
+});
+
+//実行
+$app->run();
