6de9774fe9db0962b88c6dd1150f9185956f6aed
inuzuka <h-inuzuka@biglobe.co.jp>
2014-09-13 22:57:40 +0900

    settings

---
+++ web/config.php
@@ -0,0 +1,9 @@
+<?php
+define('TEMPLATES_DIR_PATH', __DIR__.'/templates');
+define('DEBUG', true);
+define('LOG_LEVEL', \Slim\Log::FATAL);
+
+$db_settings = [
+    'driver' => 'sqlite',
+    'database' => __DIR__."/sqlite.db"
+];
